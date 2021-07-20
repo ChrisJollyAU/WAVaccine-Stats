30f059b0b9dc348ed67fbec035bea7629c775231
Christopher Jolly <chris-jolly_au@hotmail.com>
2021-07-20 21:24:31 +0800

    Add project files.

---
+++ WAVaccine/Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Serializers.NewtonsoftJson;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace WAVaccine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WAPostcode.DoPostcode();
+            WASuburb.DoSuburb();
+        }
+    }
+}
---
+++ WAVaccine/WASuburb.cs
@@ -0,0 +1,276 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Serializers.NewtonsoftJson;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAVaccine
+{
+    class WASuburb
+    {
+        public static void DoSuburb()
+        {
+            var client = new RestClient("https://wabi-australia-southeast-api.analysis.windows.net/public/reports/querydata?synchronous=true");
+            client.Timeout = -1;
+            client.UseNewtonsoftJson();
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("ActivityId", "8218c55c-bf10-4c88-ad6d-b94b1941307a");
+            request.AddHeader("RequestId", "5269c938-fb76-59d4-6de8-d272d6b94c74");
+            request.AddHeader("X-PowerBI-ResourceKey", "030227c5-6905-436e-bd82-b45af84cce64");
+            request.AddHeader("Content-Type", "application/json");
+            var body = @"{
+" + "\n" +
+@"    ""version"": ""1.0.0"",
+" + "\n" +
+@"    ""queries"": [
+" + "\n" +
+@"        {
+" + "\n" +
+@"            ""Query"": {
+" + "\n" +
+@"                ""Commands"": [
+" + "\n" +
+@"                    {
+" + "\n" +
+@"                        ""SemanticQueryDataShapeCommand"": {
+" + "\n" +
+@"                            ""Query"": {
+" + "\n" +
+@"                                ""Version"": 2,
+" + "\n" +
+@"                                ""From"": [
+" + "\n" +
+@"                                    {
+" + "\n" +
+@"                                        ""Name"": ""a"",
+" + "\n" +
+@"                                        ""Entity"": ""AIR"",
+" + "\n" +
+@"                                        ""Type"": 0
+" + "\n" +
+@"                                    }
+" + "\n" +
+@"                                ],
+" + "\n" +
+@"                                ""Select"": [
+" + "\n" +
+@"                                    {
+" + "\n" +
+@"                                        ""Measure"": {
+" + "\n" +
+@"                                            ""Expression"": {
+" + "\n" +
+@"                                                ""SourceRef"": {
+" + "\n" +
+@"                                                    ""Source"": ""a""
+" + "\n" +
+@"                                                }
+" + "\n" +
+@"                                            },
+" + "\n" +
+@"                                            ""Property"": ""Doses administered""
+" + "\n" +
+@"                                        },
+" + "\n" +
+@"                                        ""Name"": ""AIR.Doses administered""
+" + "\n" +
+@"                                    },
+" + "\n" +
+@"                                    {
+" + "\n" +
+@"                                        ""Measure"": {
+" + "\n" +
+@"                                            ""Expression"": {
+" + "\n" +
+@"                                                ""SourceRef"": {
+" + "\n" +
+@"                                                    ""Source"": ""a""
+" + "\n" +
+@"                                                }
+" + "\n" +
+@"                                            },
+" + "\n" +
+@"                                            ""Property"": ""Dose 1""
+" + "\n" +
+@"                                        },
+" + "\n" +
+@"                                        ""Name"": ""AIR.Dose 1""
+" + "\n" +
+@"                                    },
+" + "\n" +
+@"                                    {
+" + "\n" +
+@"                                        ""Measure"": {
+" + "\n" +
+@"                                            ""Expression"": {
+" + "\n" +
+@"                                                ""SourceRef"": {
+" + "\n" +
+@"                                                    ""Source"": ""a""
+" + "\n" +
+@"                                                }
+" + "\n" +
+@"                                            },
+" + "\n" +
+@"                                            ""Property"": ""Dose 2""
+" + "\n" +
+@"                                        },
+" + "\n" +
+@"                                        ""Name"": ""AIR.Dose 2""
+" + "\n" +
+@"                                    },
+" + "\n" +
+@"                                    {
+" + "\n" +
+@"                                        ""Column"": {
+" + "\n" +
+@"                                            ""Expression"": {
+" + "\n" +
+@"                                                ""SourceRef"": {
+" + "\n" +
+@"                                                    ""Source"": ""a""
+" + "\n" +
+@"                                                }
+" + "\n" +
+@"                                            },
+" + "\n" +
+@"                                            ""Property"": ""locality""
+" + "\n" +
+@"                                        },
+" + "\n" +
+@"                                        ""Name"": ""AIR.locality""
+" + "\n" +
+@"                                    }
+" + "\n" +
+@"                                ],
+" + "\n" +
+@"                                ""OrderBy"": [
+" + "\n" +
+@"                                    {
+" + "\n" +
+@"                                        ""Direction"": 1,
+" + "\n" +
+@"                                        ""Expression"": {
+" + "\n" +
+@"                                            ""Column"": {
+" + "\n" +
+@"                                                ""Expression"": {
+" + "\n" +
+@"                                                    ""SourceRef"": {
+" + "\n" +
+@"                                                        ""Source"": ""a""
+" + "\n" +
+@"                                                    }
+" + "\n" +
+@"                                                },
+" + "\n" +
+@"                                                ""Property"": ""locality""
+" + "\n" +
+@"                                            }
+" + "\n" +
+@"                                        }
+" + "\n" +
+@"                                    }
+" + "\n" +
+@"                                ]
+" + "\n" +
+@"                            },
+" + "\n" +
+@"                            ""Binding"": {
+" + "\n" +
+@"                                ""Primary"": {
+" + "\n" +
+@"                                    ""Groupings"": [
+" + "\n" +
+@"                                        {
+" + "\n" +
+@"                                            ""Projections"": [
+" + "\n" +
+@"                                                0,
+" + "\n" +
+@"                                                1,
+" + "\n" +
+@"                                                2,
+" + "\n" +
+@"                                                3,
+" + "\n" +
+@"                                            ]
+" + "\n" +
+@"                                        }
+" + "\n" +
+@"                                    ]
+" + "\n" +
+@"                                },
+" + "\n" +
+@"                                ""DataReduction"": {
+" + "\n" +
+@"                                    ""DataVolume"": 4,
+" + "\n" +
+@"                                    ""Primary"": {
+" + "\n" +
+@"                                        ""Top"": {}
+" + "\n" +
+@"                                    }
+" + "\n" +
+@"                                },
+" + "\n" +
+@"                                ""Version"": 1
+" + "\n" +
+@"                            },
+" + "\n" +
+@"                            ""ExecutionMetricsKind"": 1
+" + "\n" +
+@"                        }
+" + "\n" +
+@"                    }
+" + "\n" +
+@"                ]
+" + "\n" +
+@"            },
+" + "\n" +
+@"            ""QueryId"": """",
+" + "\n" +
+@"            ""ApplicationContext"": {
+" + "\n" +
+@"                ""DatasetId"": ""def25ac4-7ae6-4f04-a18d-b09aeab61acd"",
+" + "\n" +
+@"                ""Sources"": [
+" + "\n" +
+@"                    {
+" + "\n" +
+@"                        ""ReportId"": ""e06eb9c9-16ef-4f52-b3f3-75714ab08f87"",
+" + "\n" +
+@"                        ""VisualId"": ""79901dfa544d31e9c7ea""
+" + "\n" +
+@"                    }
+" + "\n" +
+@"                ]
+" + "\n" +
+@"            }
+" + "\n" +
+@"        }
+" + "\n" +
+@"    ],
+" + "\n" +
+@"    ""cancelQueries"": [],
+" + "\n" +
+@"    ""modelId"": 2443166
+" + "\n" +
+@"}";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            JObject des = (JObject)Newtonsoft.Json.JsonConvert.DeserializeObject(response.Content);
+            var bb = des["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"];
+
+            bb.First.Remove();
+            var cc = bb.SelectMany(v => v.First);
+            var date = DateTime.Today.AddDays(-1).ToString("yyyy-MM-dd");
+            File.WriteAllText("suburb-" + date + ".json", JsonConvert.SerializeObject(cc, Formatting.Indented));
+        }
+    }
+}
